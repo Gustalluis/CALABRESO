74169085e6b5f714748f81c9451b753dd9a444a7
gustalluis <gustavoluis2106@gmail.com>
2024-03-06 08:30:01 -0300

    ajustes

---
+++ js/animacoes.js
@@ -0,0 +1,24 @@
+/* BANNER */ 
+
+$('.banner').slick({
+    slidesToShow: 1, /*quantos qur visualizar*/
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000, /*velocidade*/ 
+  });
+
+/* SERVICO*/ 
+  $('.servico > div > div').slick({
+    slidesToShow: 3, /*quantos qur visualizar*/
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000, /*velocidade*/ 
+  });
+
+/* GALERIA*/ 
+  $('.galeria div').slick({
+    slidesToShow: 6, /*quantos qur visualizar*/
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000, /*velocidade*/ 
+  });
